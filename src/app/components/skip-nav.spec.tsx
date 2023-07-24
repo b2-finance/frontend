ca6c62269ebd72b0d15a446cb320ea0052ab13f3
prado <pradodje@gmail.com>
2023-07-23 21:33:46 -0700

    Add skip-nav, header, and self-blurring-link tests

---
+++ src/app/components/skip-nav.spec.tsx
@@ -0,0 +1,47 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import SkipNav from './skip-nav';
+
+describe('SkipNav', () => {
+  it('should render an anchor element', () => {
+    render(<SkipNav mainId="test-main-id" />);
+    const anchor = screen.getByRole('link');
+    expect(anchor).toBeInTheDocument();
+  });
+
+  it('should render an anchor element with correct text', () => {
+    render(<SkipNav mainId="test-main-id" />);
+
+    const anchor = screen.getByRole('link', {
+      name: 'Skip to main content'
+    });
+    expect(anchor).toBeInTheDocument();
+  });
+
+  // it('should initially hide the anchor element', () => {
+  //   /*
+  //     TODO: Implement this. I wanted to use getBoundingClientRect() to check that the bottom
+  //     is < 0 (i.e., element if off screen) but getBoundingRect() returns all 0s.
+  //   */
+  // });
+
+  // TODO: This is not working. document.activeElement is anchor, even after clicking.
+  //   it('should move focus to main element when clicked', async () => {
+  //     const user = userEvent.setup();
+  //     const mainId = 'test-main-id';
+
+  //     render(
+  //       <div>
+  //         <SkipNav mainId={mainId} />
+  //         <main tabIndex={0} id={mainId} />
+  //       </div>
+  //     );
+  //     const anchor = screen.getByRole('link', {
+  //       name: 'Skip to main content'
+  //     });
+  //     const main = screen.getByRole('main');
+
+  //     await user.click(anchor);
+  //     await waitFor(() => expect(document.activeElement).toEqual(main));
+  //   });
+});

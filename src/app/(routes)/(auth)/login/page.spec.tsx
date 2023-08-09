28c67341900f122c80a5cf3ad63d720cd84d3800
prado <pradodje@gmail.com>
2023-08-08 21:28:45 -0700

    Add tests for auth pages

---
+++ src/app/(routes)/(auth)/login/page.spec.tsx
@@ -0,0 +1,24 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import LoginPage from './page';
+import routes from '@/utils/routes';
+
+jest.mock('next/navigation', () => ({
+  useRouter: () => ({
+    push: jest.fn()
+  })
+}));
+
+describe('LoginPage', () => {
+  it('should render a logo link to the home page', () => {
+    render(<LoginPage />);
+    const logo = screen.getByRole('link', { name: 'B2' });
+    expect(logo).toHaveAttribute('href', routes.home);
+  });
+
+  it('should render a signup form', () => {
+    render(<LoginPage />);
+    const form = screen.getByRole('heading', { name: 'Log In' });
+    expect(form).toBeInTheDocument();
+  });
+});
